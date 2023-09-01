25efc32bcc8e05b4952cbffde5bb52c9f3422e6b
vikas pundir <vikaspundiruser@gmail.com>
2023-09-02 03:32:48 +0530

    Almost  done

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
 const containers = document.querySelector(".containers");
 const detailPage = document.querySelector(".detail-page");
 const homePage = document.querySelector(".home-page");
-const modeBtn = document.querySelector(".mode-switch")
+const modeBtn = document.querySelector(".mode-switch");
+const selectMenu = document.querySelector(".select-menu");
 
-
-async function fetchData() {
+async function fetchDataAll() {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
   return data;
 }
 
 async function displayData() {
-  const data = await fetchData();
+  const data = await fetchDataAll();
   data.forEach((cardData) => {
     displayCards(cardData);
   });
+}
+
+displayData();
+
+/* FILTER BY REGION */
+
+async function fetchRegionData(region) {
+  containers.innerHTML = "";
+  if (region.value === "all") {
+    displayData();
+  } else {
+    const response = await fetch(
+      `https://restcountries.com/v3.1/region/${region.value}`
+    );
+    const data = await response.json();
+    data.forEach((cardData) => {
+      displayCards(cardData);
+    });
+  }
+}
+
+/* SEARCH BY INPUT */
+
+async function fetchSearchData(event) {
+  if (event.keyCode == 13) {
+    if (event.target.value == "") {
+      displayData();
+    } else {
+      const response = await fetch(
+        `https://restcountries.com/v3.1/name/${event.target.value}`
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data.status != 404) {
+        containers.innerHTML = "";
+        data.forEach((cardData) => {
+          displayCards(cardData);
+        });
+      }
+    }
+  }
 }
 
 function displayCards(cardData) {
@@ -30,8 +71,6 @@ function displayCards(cardData) {
   containers.insertAdjacentHTML("beforeend", html);
 }
 
-displayData();
-
 document.addEventListener("click", (e) => {
   let container = e.target.closest(".container");
   if (container) {
@@ -41,7 +80,7 @@ document.addEventListener("click", (e) => {
 });
 
 async function getDetailPageData(countryName) {
-  const detailPageData = await fetchData();
+  const detailPageData = await fetchDataAll();
   detailPageData.forEach((detail) => {
     if (detail.name.common === countryName) {
       showDetailPage(detail);
@@ -50,8 +89,6 @@ async function getDetailPageData(countryName) {
 }
 
 function showDetailPage(detail) {
-  console.log(detail)
-
   const detailHtml = `
   <div class="back"  onClick="goBack()">
   <button class="back-btn"><i class="fa-solid fa-arrow-left"></i>Back</button>
@@ -64,72 +101,87 @@ function showDetailPage(detail) {
           <h1 class="country-name">${detail.name.common}</h1>
           <div class="small-details">
               <div class="left-small-details">
-                  <p class="small-detail"><span>Native Name: </span>${Object.values(detail.name.nativeName)[0].common}</p>
-                  <p class="small-detail"><span>Population: </span>${detail.population}</p>
-                  <p class="small-detail"><span>Region: </span>${detail.region}</p>
-                  <p class="small-detail"><span>Sub Region: </span>${detail.subregion}</p>
-                  <p class="small-detail"><span>Capital: </span>${detail.capital}</p>
+                  <p class="small-detail"><span>Native Name: </span>${
+                    Object.values(detail.name.nativeName)[0].common
+                  }</p>
+                  <p class="small-detail"><span>Population: </span>${
+                    detail.population
+                  }</p>
+                  <p class="small-detail"><span>Region: </span>${
+                    detail.region
+                  }</p>
+                  <p class="small-detail"><span>Sub Region: </span>${
+                    detail.subregion
+                  }</p>
+                  <p class="small-detail"><span>Capital: </span>${
+                    detail.capital
+                  }</p>
               </div>
               <div class="right-small-details">
-                  <p class="small-detail"><span>Top Level Domain: </span>${detail.tld[0]}</p>
+                  <p class="small-detail"><span>Top Level Domain: </span>${
+                    detail.tld[0]
+                  }</p>
                   <p class="small-detail"><span>Currencies: </span>${
                     Object.values(detail.currencies)[0].name
                   }</p>
-                  <p class="small-detail"><span>Languages: </span>${Object.values(Object.values(detail.languages)).join(
-                    ", "
-                  )}</p>
+                  <p class="small-detail"><span>Languages: </span>${Object.values(
+                    Object.values(detail.languages)
+                  ).join(", ")}</p>
               </div>
           </div>
-          <p><span>Border Countries:</span><span></span><span></span><span></span></p>
+          <p><span>Border Countries: </p>
       </div>
 
   </div>`;
-  detailPage.classList.remove("hide");
-  homePage.classList.add("hide");
+  detailPage.style.display = "flex";
+  homePage.style.display = "none";
   detailPage.innerHTML = detailHtml;
 }
 
 function goBack() {
-  detailPage.classList.add("hide");
-  homePage.classList.remove("hide");
+  detailPage.style.display = "none";
+  homePage.style.display = "block";
 }
-
-
-
 
 /* DARK AND LIGHT MODE TOGGLE */
 
 modeBtn.addEventListener("click", () => {
-  let currLightMode = modeBtn.classList.contains("light")
-  let currDarkMode = modeBtn.classList.contains("dark")
-  if(currLightMode) {
-    modeBtn.classList.remove("light")
-    modeBtn.classList.add("dark")
-    homePage.style.backgroundColor = "hsl(207, 26%, 17%)"
-    detailPage.style.backgroundColor = "hsl(207, 26%, 17%)"
-    detailPage.style.color = "hsl(0, 0%, 100%)"
-    homePage.style.color = "hsl(0, 0%, 100%)"
-    document.querySelector(".navbar").style.backgroundColor = "hsl(209, 23%, 22%)"
-    document.querySelector(".input").classList.add("input-dark-mode")
-    document.querySelector(".navbar").style.color = "hsl(0, 0%, 100%)"
-    document.querySelector(".input-bar").classList.add("input-bar-dark")
-    document.querySelector(".select-menu").classList.add("select-menu-dark")
-    document.querySelectorAll(".container").forEach((container) => container.style.backgroundColor = "hsl(209, 23%, 22%)")
+  let currLightMode = modeBtn.classList.contains("light");
+  let currDarkMode = modeBtn.classList.contains("dark");
+  if (currLightMode) {
+    modeBtn.classList.remove("light");
+    modeBtn.classList.add("dark");
+    homePage.style.backgroundColor = "hsl(207, 26%, 17%)";
+    detailPage.style.backgroundColor = "hsl(207, 26%, 17%)";
+    detailPage.style.color = "hsl(0, 0%, 100%)";
+    homePage.style.color = "hsl(0, 0%, 100%)";
+    document.querySelector(".navbar").style.backgroundColor =
+      "hsl(209, 23%, 22%)";
+    document.querySelector(".input").classList.add("input-dark-mode");
+    document.querySelector(".navbar").style.color = "hsl(0, 0%, 100%)";
+    document.querySelector(".input-bar").classList.add("input-bar-dark");
+    document.querySelector(".select-menu").classList.add("select-menu-dark");
+    document
+      .querySelectorAll(".container")
+      .forEach(
+        (container) => (container.style.backgroundColor = "hsl(209, 23%, 22%)")
+      );
+  } else if (currDarkMode) {
+    modeBtn.classList.remove("dark");
+    modeBtn.classList.add("light");
+    homePage.style.backgroundColor = "hsl(0, 0%, 98%)";
+    detailPage.style.backgroundColor = "hsl(0, 0%, 98%)";
+    detailPage.style.color = "hsl(200, 15%, 8%)";
+    homePage.style.color = "hsl(200, 15%, 8%)";
+    document.querySelector(".navbar").style.backgroundColor = "white";
+    document.querySelector(".input").classList.remove("input-dark-mode");
+    document.querySelector(".navbar").style.color = " hsl(200, 15%, 8%)";
+    document.querySelector(".input-bar").classList.remove("input-bar-dark");
+    document.querySelector(".select-menu").classList.remove("select-menu-dark");
+    document
+      .querySelectorAll(".container")
+      .forEach(
+        (container) => (container.style.backgroundColor = " hsl(0, 0%, 100%)")
+      );
   }
-  else if(currDarkMode) {
-    modeBtn.classList.remove("dark")
-    modeBtn.classList.add("light")
-    homePage.style.backgroundColor = "hsl(0, 0%, 98%)"
-    detailPage.style.backgroundColor = "hsl(0, 0%, 98%)"
-    detailPage.style.color = "hsl(200, 15%, 8%)"
-    homePage.style.color = "hsl(200, 15%, 8%)"
-    document.querySelector(".navbar").style.backgroundColor = "white"
-    document.querySelector(".input").classList.remove("input-dark-mode")
-    document.querySelector(".navbar").style.color = " hsl(200, 15%, 8%)"
-    document.querySelector(".input-bar").classList.remove("input-bar-dark")
-    document.querySelector(".select-menu").classList.remove("select-menu-dark")
-    document.querySelectorAll(".container").forEach((container) => container.style.backgroundColor = " hsl(0, 0%, 100%)")
-  }
- 
- 
-})
+});
